27143a2242719b64c53e543a4d9ea2aa0db6bed7
Jesse Piccione <72052332+JessePiccione@users.noreply.github.com>
2024-07-21 20:11:52 +0000

    finished working announcement component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,21 +7,17 @@ function Header(){
         <header>
             <OffCanvas />
             <div className='container-fluid m-0 p-0 z-3'>
-                <div className='row bg-black bg-gradient text-white m-0 p-0' style={{'--bs-bg-opacity':'0.85'}}>
-                    <div className = 'col' >
-                        <div className='text-center'>
-                            <img className='rounded-profile m-3' style={{minHeight:'250px', minWidth:'250px'}} src="static/img/resume_site_profile.jpg" alt='round center profile'></img>
-                            <h1>
-                                <DelayedCharacters className='flip' word='Jesse Piccione'/>
-                            </h1>
-                            <h2>
-                                <DelayedCharacters className='zip' word='Software Engineer'/>
-                            </h2>
-                        </div>
-                        <div className='mb-3'>
-                            <AnnoucementBanner title='Welcome to the React Side!'/>
-                        </div>
+                <div className='row bg-black bg-gradient text-white justify-content-md-center' style={{'--bs-bg-opacity':'0.85'}}>    
+                    <div className='col-12 col-xxl-7 text-center bg-gradient bg-black rounded border-0 shadow-sm' style={{'--bs-bg-opacity':'0'}}>
+                        <img className='rounded-profile m-3' style={{minHeight:'250px', minWidth:'250px'}} src="static/img/resume_site_profile.jpg" alt='round center profile'></img>
+                        <h1>
+                            <DelayedCharacters className='flip' word='Jesse Piccione'/>
+                        </h1>
+                        <h2>
+                            <DelayedCharacters className='zip' word='Software Engineer'/>
+                        </h2>
                     </div>
+                   <AnnoucementBanner/>
                 </div>
             </div>  
         </header>
