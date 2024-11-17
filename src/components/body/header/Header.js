2dda5dc8abd8c7813bc96818d0223ded5a0ba1b8
Jesse Piccione <72052332+JessePiccione@users.noreply.github.com>
2024-11-17 19:39:43 +0000

    changing and updating film

--- src/components/body/header/Header.js
+++ src/components/body/header/Header.js
@@ -10,7 +10,7 @@ function Header(){
             <OffCanvas />
             <div className='container-fluid z-3'>
                 <div className='row bg-black bg-gradient justify-content-md-center' style={{'--bs-bg-opacity':'0.75'}}>    
-                    <div className='col-12 col-xxl-7 px-0'>
+                    <div className='col-12 col-xxl-10 px-0'>
                         <div className='card text-white bg-black text-center rounded border-0 shadow-sm'  style={{'--bs-bg-opacity':'0'}}>
                             <div className='card-body rounded border-0 m-3 shadow-sm'>
                                 <img className='rounded-profile my-3' style={{minHeight:'250px', minWidth:'250px'}} src="static/img/resume_site_profile.jpg" alt='round center profile'></img>
