8de737ba9a578a4b754d01a980b6d7dd5133c2c4
Jesse Piccione <72052332+JessePiccione@users.noreply.github.com>
2024-12-12 02:53:21 +0000

    jessepiccione.info correcting favicon on error page

--- src/components/body/nav/Nav.js
+++ src/components/body/nav/Nav.js
@@ -2,7 +2,7 @@ import NavList from './NavList.js'
 import {Link} from 'react-router-dom'
 function Nav(){
     return (
-    <div className='container-fluid sticky-top primary-color bg-gradient d-flex justify-content-center'>
+    <div className='container-fluid sticky-top primary-color bg-gradient'>
         <div className='container'>
             <nav className='navbar col-12 navbar-expand-md shadow-sm' data-bs-theme='dark'>
                 <Link className='navbar-brand' to='/'>JessePiccione.info</Link>
