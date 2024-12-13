c0cfa2766313efdefc1385762ad3f52dc7389f48
Jesse Piccione <jessepiccione@yahoo.com>
2024-12-12 19:41:30 -0500

    Update Footer.js

--- src/components/body/footer/Footer.js
+++ src/components/body/footer/Footer.js
@@ -2,7 +2,7 @@ import NavList from '../nav/NavList'
 import LogoBanner from '../partials/LogoBanner'
 const Footer = () =>(
     <footer className='container-fluid text-light primary-color bg-gradient'>
-        <div className='container pt-5'>
+        <div className='container pt-4'>
             <LogoBanner/>
             <nav className="navbar col-12 navbar-expand-md justify-content-center" data-bs-theme='dark'>
                 <NavList/>
