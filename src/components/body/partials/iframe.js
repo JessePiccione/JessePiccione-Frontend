aa06c90a65f130c49b351a6614df3a36e02d44a6
Jesse Piccione <jesse.piccione@spencergifts.com>
2024-12-17 11:19:20 -0500

    Update iframe.js
    Updating how iframes are handled in the interface to reduce console warnings

--- src/components/body/partials/iframe.js
+++ src/components/body/partials/iframe.js
@@ -2,7 +2,14 @@ const iframe = props => {
     return (
         <div className='ratio ratio-16x9'>
             <div className='position-absolute col-12 placeholder placeholder-wave rounded'></div>
-            <iframe className='embed-responsive-item rounded shadow-sm' src={props.URL} title="YouTube Video"  allowFullScreen/>
+            <iframe 
+                allowFullScreen
+                src={props.URL}
+                title="YouTube Video"
+                frameborder="0"
+                className='embed-responsive-item rounded shadow-sm' 
+                allow="accelerometer: autoplay; encrypted-media; gyroscope; picture-in-picture"
+            />
         </div>
     )
 }
