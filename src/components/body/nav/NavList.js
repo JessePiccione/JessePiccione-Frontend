04e536660003b855161624fe25331bc2e6321b5c
Jesse Piccione <jesse.piccione@spencergifts.com>
2024-12-17 14:21:38 -0500

    Update NavList.js
    changed navlist to be text-start instead of defaulting to text-center

--- src/components/body/nav/NavList.js
+++ src/components/body/nav/NavList.js
@@ -2,7 +2,7 @@ import NavItem from './NavItem.js'
 const viewNames = ['Home','Experience', 'Education', 'Projects', 'Skills', 'Awards']
 function NavList(){
     return (
-        <ul className="navbar-nav d-flex flex-column flex-md-row px-2">
+        <ul className="navbar-nav d-flex flex-column flex-md-row text-start px-2">
             {viewNames.map((val,index)=> {
                 let i = index;
                 return ( 
